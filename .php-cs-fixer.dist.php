8c1f1799800a824042e0dc3cc17c890384f258a7
Steven Berg <steven@stevenberg.net>
2023-04-25 07:52:29 -0400

    Update development packages

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,12 +4,19 @@ $finder = PhpCsFixer\Finder::create()->in(__DIR__);
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@PHP80Migration' => true,
+        '@PHP82Migration' => true,
         '@PhpCsFixer' => true,
         'concat_space' => [
             'spacing' => 'one',
         ],
         'mb_str_functions' => true,
+        'ordered_imports' => [
+            'imports_order' => [
+                'const',
+                'class',
+                'function',
+            ],
+        ],
         'phpdoc_add_missing_param_annotation' => false,
         'php_unit_internal_class' => false,
         'php_unit_test_class_requires_covers' => false,
